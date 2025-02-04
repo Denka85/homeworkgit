eb032cb2e9c7083a3a8d3f7221e1739997f05ba9
Denis <Mypchystory@yandex.ru>
2025-02-04 15:12:23 +0300

    Реализация функций Функция get_mask_card_number  принимает на вход номер карты в виде числа и возвращает маску номера по правилу XXXX XX** **** XXXX
    get_mask_account
     принимает на вход номер счета в виде числа и возвращает маску номера по правилу
    **XXXX
    .

--- test/main.py
+++ test/main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.wiget import get_date, mask_account_card
+
 
 if __name__ == "__main__":
     card_num = 7000792289606361
@@ -7,6 +7,4 @@ if __name__ == "__main__":
 
     print("Маска карты:", get_mask_card_number(card_num))
     print("Маска счета:", get_mask_account(account_num))
-    print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Maestro 7000792289606361"))
-    print(mask_account_card("Счет 73654108430135874305"))
+
