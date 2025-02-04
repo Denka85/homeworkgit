ecdbbb2424b6b6b024fe88c04ab6a3a924195f65
Denis <Mypchystory@yandex.ru>
2025-02-04 15:52:45 +0300

    дабавлена строка print get_data выводит строку дата

--- test/main.py
+++ test/main.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 7000792289606361"))
     print(mask_account_card("Maestro 7000792289606361"))
     print(mask_account_card("Счет 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
 
