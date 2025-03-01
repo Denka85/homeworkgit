3663dd9ef3f1bba96c959295cb58686b1b015625
Denis <Mypchystory@yandex.ru>
2025-03-01 19:41:57 +0300

    написал тесты на masks.py processing.py widget.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state
-from src.processing import sort_by_date
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
 
-if __name__ == "__main__":
+def test_get_mask_card_number():
+    card_number = 7000792289606361
+    assert get_mask_card_number(card_number) == "7000 79** **** 6361"
 
+def test_get_mask_account():
+    account_number = 73654108430135874305
+    assert get_mask_account(account_number) == "**4305"
     '''данные для маскировки номера карт и аккаунта'''
     card_num = 7000792289606361
     account_num = 73654108430135874305
 
     '''вывод маски карт и даты'''
     print("Маска карты:", get_mask_card_number(card_num))
-    print("Маска счета:", get_mask_account(account_num))
+    print("mainМаска счета:", get_mask_account(account_num))
     print(mask_account_card("Visa Platinum 7000792289606361"))
     print(mask_account_card("Maestro 7000792289606361"))
     print(mask_account_card("Счет 73654108430135874305"))
@@ -24,11 +27,3 @@ operations_data = [
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
-
-'''Получаем операции со статусом EXECUTED'''
-executed_ops = filter_by_state(operations_data)
-print(executed_ops)
-
-'''Сортируем по дате (по убыванию)'''
-sorted_ops_desc = sort_by_date(operations_data)
-print(sorted_ops_desc)
